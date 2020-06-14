e6d3010baab9abc9b8f371bb7f2617fcce64d285
mocklee <emockler@ucsc.edu>
2020-06-13 23:48:28 -0700

    made heroText steps a class variable for appropriate modularity

--- lib/landing/hero/heroText.dart
+++ lib/landing/hero/heroText.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
 class HeroText extends StatefulWidget {
+  HeroText(
+      {Key key,
+      this.step1 = const [
+        'Step 1:\n',
+        'Let the recepient know who you are! Fill in your details below.'
+      ],
+      this.step2 = const [
+        'Step 2:\n',
+        'Find your cause by searching through submitted topics, or add a new one!'
+      ],
+      this.step3 = const [
+        'Step 3:\n',
+        'Tap on a card to open the email. Add your voice by hitting the “SEND” button.'
+      ]})
+      : super(key: key);
+
+  final List step1;
+  final List step2;
+  final List step3;
+
   @override
   _HeroTextState createState() => _HeroTextState();
 }
@@ -9,42 +29,31 @@ class _HeroTextState extends State<HeroText> {
   double opacity = 1;
   int opacityStep = 1;
 
-  // initialize on first step
-  String stepTitle = 'Step 1:\n';
-  String stepDescription =
-      'Let the recepient know who you are! Fill in your details below.';
-  List step1 = [
-    'Step 1:\n',
-    'Let the recepient know who you are! Fill in your details below.'
-  ];
-  List step2 = [
-    'Step 2:\n',
-    'Find your cause by searching through submitted topics, or add a new one!'
-  ];
-  List step3 = [
-    'Step 3:\n',
-    'Tap on a card to open the email. Add your voice by hitting the “SEND” button.'
-  ];
+  String stepTitle;
+  String stepDescription;
 
   @override
   void initState() {
     super.initState();
+    // initialize on first step
+    stepTitle = widget.step1[0];
+    stepDescription = widget.step1[1];
     changeOpacity();
   }
 
   void changeOpacity() {
-    Future.delayed(Duration(seconds: 8), () {
+    Future.delayed(Duration(seconds: 6), () {
       setState(() {
         switch (opacityStep) {
           case 1:
             {
               opacity = 0;
-              Future.delayed(Duration(milliseconds: 1500), () {
+              Future.delayed(Duration(milliseconds: 1200), () {
                 setState(() {
                   opacity = 1;
                   opacityStep = 2;
-                  stepTitle = step2[0];
-                  stepDescription = step2[1];
+                  stepTitle = widget.step2[0];
+                  stepDescription = widget.step2[1];
                 });
               });
               continue recursion;
@@ -56,8 +65,8 @@ class _HeroTextState extends State<HeroText> {
                 setState(() {
                   opacity = 1;
                   opacityStep = 3;
-                  stepTitle = step3[0];
-                  stepDescription = step3[1];
+                  stepTitle = widget.step3[0];
+                  stepDescription = widget.step3[1];
                 });
               });
               continue recursion;
@@ -69,8 +78,8 @@ class _HeroTextState extends State<HeroText> {
                 setState(() {
                   opacity = 1;
                   opacityStep = 1;
-                  stepTitle = step1[0];
-                  stepDescription = step1[1];
+                  stepTitle = widget.step1[0];
+                  stepDescription = widget.step1[1];
                 });
               });
               continue recursion;
