afbc49cd35f5faa7208f1b62ee041df5390a52f6
mocklee <mock7ee@gmail.com>
2020-09-25 23:35:53 -0700

    added method to generate email list from Firestore snapshot

--- lib/email/emailRelay.dart
+++ lib/email/emailRelay.dart
@@ -1,24 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//** Adds new email object to Firestore collection */
 Future<void> addEmail(email) {
   final emails = FirebaseFirestore.instance.collection('emails');
   return emails.add({
     'anonymousID': email.anonymousID,
     'causeSummary': email.causeSummary,
-    'emailbody': email.emailBody,
-    'emailDestination': [email.emailDestination],
-    'emailSubject': email.subject,
+    'body': email.emailBody,
+    'destination': [email.emailDestination],
+    'subject': email.subject,
     'sentClickCount': 0,
     'tags': [email.tag],
     'title': email.title,
   });
 }
 
-//** Returns snapshot of Firestore collection */
-Stream<QuerySnapshot> loadFromCollection() {
+//** Returns list of emails with given parameters */
+List<Email> getEmailsFromQuery(QuerySnapshot snapshot) {
+  return snapshot.docs.map((DocumentSnapshot doc) {
+    return Email.fromSnapshot(doc);
+  }).toList();
+}
+
+//** Returns snapshot of email collection */
+Stream<QuerySnapshot> loadFromCollection(int limit) {
   return FirebaseFirestore.instance
       .collection('emails')
       .orderBy('sentClickCount', descending: true)
-      .limit(50)
+      .limit(limit)
       .snapshots();
 }
