a8346b71a98f08f4f1cdd3d3ed102c7af3e18f65
mocklee <mock7ee@gmail.com>
2020-09-26 23:34:45 -0700

    switched app persistent data to localstorage
    also boilerplated desktop email card layout further

--- lib/landing/emailList/emailCard/layouts/desktopEmail.dart
+++ lib/landing/emailList/emailCard/layouts/desktopEmail.dart
@@ -1,7 +1,9 @@
+import 'package:communique/email/email.dart';
 import 'package:flutter/material.dart';
 
 class DesktopEmail extends StatefulWidget {
-  DesktopEmail({Key key}) : super(key: key);
+  final Email currentEmail;
+  DesktopEmail({Key key, this.currentEmail}) : super(key: key);
 
   @override
   _DesktopEmailState createState() => _DesktopEmailState();
@@ -10,8 +12,25 @@ class DesktopEmail extends StatefulWidget {
 class _DesktopEmailState extends State<DesktopEmail> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
+    return Card(
+      child: SizedBox(
+          width: 375,
+          height: 200,
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ListTile(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 0,
+                    ),
+                    title: Text(widget.currentEmail.title,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ))),
+                Row(mainAxisAlignment: MainAxisAlignment.start)
+              ])),
     );
   }
 }
--- lib/landing/inputUpdater.dart
+++ lib/landing/inputUpdater.dart
@@ -1,26 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+import 'dart:convert';
 
 class InputUpdater extends ChangeNotifier {
-  // public
-  String name;
-  String location;
-  List<bool> repLevel;
+  static String name;
+  static String location;
+  static List<bool> repLevel;
+  static List<DocumentReference> sentEmails;
 
   void updateName(String userName) {
+    window.localStorage['communiqueUserName'] = userName;
+
     name = userName;
     notifyListeners();
   }
 
   void updateLocation(String userLocation) {
-    location = userLocation;
-    print(location);
+    window.localStorage['communiqueUserLocation'] = userLocation;
 
+    location = userLocation;
     notifyListeners();
   }
 
   void updateRepLevel(List<bool> userSelectedLevels) {
     repLevel = userSelectedLevels;
-    print(repLevel);
     notifyListeners();
+  }
+
+  void updateSentEmailCache(DocumentReference emailReference) {
+    if (window.localStorage.containsKey('communiqueSentEmailReferences')) {
+      sentEmails =
+          jsonDecode(window.localStorage['communiqueSentEmailReferences']);
+    }
+    sentEmails.add(emailReference);
+    window.localStorage['communiqueSentEmailReferences'] =
+        jsonEncode(sentEmails);
   }
 }
