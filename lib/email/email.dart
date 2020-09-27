d7aa87cabc5c94980453ea54705e0623ff6d7318
mocklee <mock7ee@gmail.com>
2020-09-26 18:48:45 -0700

    removed fingerprintjs, in favor of firebase anonymous auth

--- lib/email/email.dart
+++ lib/email/email.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:communique/email/moderationReport.dart';
+import 'package:communique/email/moderation/moderationReport.dart';
 
 class Email {
   final String anonymousID;
--- lib/landing/anonymousFingerprint.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fingerprintjs/fingerprintjs.dart';
-
-class AnonymousFingerprint {
-  static String fingerprint;
-
-  static void create() async {
-    fingerprint = await Fingerprint.getHash();
-  }
-}
