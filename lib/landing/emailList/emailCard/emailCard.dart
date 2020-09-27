e893fafbd36ecfd1a8356a0afa6258f5ea79b239
mocklee <mock7ee@gmail.com>
2020-09-26 22:30:40 -0700

    boilerplated desktop layout email card

--- lib/landing/emailList/emailCard/emailCard.dart
+++ lib/landing/emailList/emailCard/emailCard.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:communique/landing/emailList/emailListItem.dart';
 import 'package:communique/landing/emailList/emailCard/layouts/desktopEmail.dart';
 
-class EmailCard extends StatelessWidget {
-  EmailCard({Key key}) : super(key: key);
+class EmailCard implements EmailListItem {
+  Widget buildTagHeader(BuildContext context) => null;
 
-  @override
-  Widget build(BuildContext context) {
+  Widget buildEmailCard(BuildContext context) {
     return LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
       if (constraints.maxWidth > 1200) {
--- lib/landing/emailList/emailCard/layouts/desktopEmail.dart
+++ lib/landing/emailList/emailCard/layouts/desktopEmail.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DesktopEmail extends StatefulWidget {
+  DesktopEmail({Key key}) : super(key: key);
+
   @override
   _DesktopEmailState createState() => _DesktopEmailState();
 }
 
-class _DesktopEmailState extends State<DesktopEmail> {}
+class _DesktopEmailState extends State<DesktopEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
